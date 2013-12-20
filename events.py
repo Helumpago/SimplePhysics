c3d360b227f3c4306d6a69fd8d8039bb76468d92
Helumpago <wrosenberger@live.com>
2013-12-20 13:55:12 -0700

    Simplified EVENT objects

--- events.py
+++ events.py
@@ -7,24 +7,10 @@
 " Base event class
 """
 class EVENT(object):
-	"""
-	" CONSTRUCTOR
-	"""
-	def __init__(self, callback = None):
-		self.cb = callback # Function to call when the event is fired
-
-	"""
-	" Run the callback
-	"""
-	def callback(self):
-		self.cb()
+	pass
 
 """
 " Fired when Pygame asks to exit
 """
 class QUIT(EVENT):
-	def __init__(self, callback = None):
-		EVENT.__init__(self, callback)
-
-	def callback(self):
-		self.cb(self)
+	pass
